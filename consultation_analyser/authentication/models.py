5d7a4e37e73095ed4e47c09f0e2e3a36039c3daf
Duncan Brown <duncanjbrown@gmail.com>
2024-04-11 16:32:00 +0100

    Add custom User model
    - Put it in a separate app so it can be shared by the frontend and the
      admin interface, which will in turn be a separate app
    - No username — have email as the primary identifier for users
    - Most users won't need passwords and Django needs the column for the
      admin interface, so default to... a uuid4?
    - Punt the has_perm and has_module_perms questions for now and have both
      return True for everything

---
+++ consultation_analyser/authentication/models.py
@@ -0,0 +1,47 @@
+import uuid
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.validators import validate_email
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        # If there's no pw, assign a long random one that won't be used
+        if password is None:
+            password = uuid.uuid4()
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password=password,
+        )
+
+        user.full_clean()
+
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    modified_at = models.DateTimeField(editable=False, auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    # boilerplate required by django admin
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
