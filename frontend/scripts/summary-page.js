7d3c3a9c68e0c6c89b040803238b9dce30ab1cc1
Kevin Etchells <kevetchells@hotmail.com>
2024-07-11 13:08:19 +0100

    Create demo scatter chart

--- frontend/scripts/summary-page.js
+++ frontend/scripts/summary-page.js
@@ -28,8 +28,8 @@ class DonutChart extends HTMLElement {
             if (!firstTime) {
                 const box = this.getBoundingClientRect();
                 chart.resize({
-                    height: box.width,
-                    width: box.height
+                    height: box.height,
+                    width: box.width
                 });
             }
         };
@@ -123,3 +123,84 @@ class BarAnimation extends HTMLElement {
     }
 }
 customElements.define('bar-animation', BarAnimation);
+
+
+
+// scatter chart
+class ScatterChart extends HTMLElement {
+    connectedCallback() {
+
+        // @ts-ignore (echarts global)
+        let chart = echarts.init(this, null, {renderer: 'svg'});
+
+        /**
+         * Size the chart and chart container
+         */
+        const sizeChart = () => {
+            const parentWidth = this.parentElement?.scrollWidth || 1;
+            this.style.width = `${parentWidth}px`;
+            this.style.height = `${parentWidth * 0.5}px`;
+            const box = this.getBoundingClientRect();
+            chart.resize({
+                height: box.height,
+                width: box.width
+            });
+        };
+        sizeChart();
+        window.addEventListener('resize', sizeChart);
+
+        const data = [
+            [10.0, 8.04, "Test item"],
+            [8.07, 6.95],
+            [13.0, 7.58],
+            [9.05, 8.81],
+            [11.0, 8.33],
+            [14.0, 7.66],
+            [13.4, 6.81],
+            [10.0, 6.33],
+            [14.0, 8.96],
+            [12.5, 6.82],
+            [9.15, 7.2],
+            [11.5, 7.2],
+            [3.03, 4.23],
+            [12.2, 7.83],
+            [2.02, 4.47],
+            [1.05, 3.33],
+            [4.05, 4.96],
+            [6.03, 7.24],
+            [12.0, 6.26],
+            [12.0, 8.84],
+            [7.08, 5.82],
+            [5.02, 5.68]
+        ];
+
+        const options = {
+            xAxis: {},
+            yAxis: {},
+            series: [
+              {
+                symbolSize: 10, // size of the dots
+                data: data,
+                type: 'scatter',
+                itemStyle: {
+                    color: '#C50878'
+                }
+              }
+            ],
+            tooltip: {
+                trigger: 'item',
+                formatter: function (params) {
+                    return `
+                        X: ${params.value[0]}<br/>
+                        Y: ${params.value[1]}<br/>
+                        Info: ${params.value[2]}<br/>
+                        Anything you like can be shown here
+                    `;
+                }
+            },
+          };
+          chart.setOption(options);
+
+    }
+}
+customElements.define('scatter-chart', ScatterChart);
